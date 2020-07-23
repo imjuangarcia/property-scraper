950d6f287b5f12d07dbda564a84c822e84e9be49
Juan Martín García <hello@juangarcia.com.ar>
2020-07-23 10:45:23 -0300

    Added meli scraper

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const request = require('request');
 const cheerio = require('cheerio');
+const { parse } = require('path');
 const app = express();
 
 app.get('/scrape/argenprop/:pageNumber', function (req, res) {
@@ -78,8 +79,6 @@ app.get('/scrape/properati/:pageNumber', function (req, res) {
         const ambients = commonInfo.includes('ambiente') ? commonInfo.charAt(commonInfo.indexOf('ambiente') - 2).trim() : '';
         const area = $(this).find('.area').text().trim();
 
-        console.log(ambients);
-
         // Push the properties to the array
         properties.push({
           url: url,
@@ -98,6 +97,54 @@ app.get('/scrape/properati/:pageNumber', function (req, res) {
     }
 
     fs.appendFile('properati.json', JSON.stringify(properties, null, 4), function (err) {
+      console.log('File successfully written.');
+    })
+
+    res.send('Check your console');
+  });
+});
+
+app.get('/scrape/meli/:pageNumber', function (req, res) {
+  // Casa alquiler zona norte, menor precio
+  url = `https://inmuebles.mercadolibre.com.ar/casas/alquiler/bsas-gba-norte/_Desde_${req.params.pageNumber + 48}_OrderId_PRICE`;
+
+  request(url, function (error, response, html) {
+    // Array to hold the properties
+    const properties = [];
+    
+    if (!error) {
+      const $ = cheerio.load(html);
+
+      // To get the info on the page
+      $('.results-item').filter(function() {
+        const url = $(this).find('.images-viewer').attr('item-url');
+        const image = $(this).find('.item__image img').attr('src') || $(this).find('.loading').attr('data-src');
+        const title = $(this).find('.item__image img').attr('alt') || $(this).find('.loading').attr('alt');
+        const price = $(this).find('.price__fraction').text().split('.').join("");
+        const address = $(this).find('.item_subtitle span').text().trim();
+        const location = $(this).find('.item__title').text().trim();
+        const commonInfo = $(this).find('.item__attrs').text();
+        const ambients = commonInfo.charAt(commonInfo.indexOf('|') + 2);
+        const area = commonInfo.substring(0, commonInfo.indexOf('m²'));
+
+        console.log(area);
+
+        // Push the properties to the array
+        properties.push({
+          url: url,
+          image: image,
+          price: parseInt(price),
+          address: address,
+          title: title,
+          location: location,
+          description: title,
+          ambients: parseInt(ambients),
+          area: parseInt(area),
+        });
+      });
+    }
+
+    fs.appendFile('meli.json', JSON.stringify(properties, null, 4), function (err) {
       console.log('File successfully written.');
     })
 
